aeb057f04d6de9ff998446668698b66e894642f4
wogong <wogong38@gmail.com>
2020-03-15 19:24:49 +0800

    remove ga

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,12 +19,6 @@ export default ({ titlePre = '' }) => {
     <header className={styles.header}>
       <Head>
 
-        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KCBCP6D');`}}  />
-
         <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
         <meta
           name="description"
@@ -36,7 +30,6 @@ export default ({ titlePre = '' }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
-      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
       <ul>
         {navItems.map(({ label, page, link }) => (
           <li key={label}>
