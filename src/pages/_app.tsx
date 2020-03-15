256349a79071e4f02260e6d624430db86418ff66
wogong <wogong38@gmail.com>
2020-03-15 19:00:55 +0800

    fix error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ export default ({ Component, pageProps }) => (
     <Component {...pageProps} />
 
     <footer>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-34308107-1"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
